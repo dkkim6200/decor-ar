04ae9d247d87d7ea6b959164b50221b262567c65
Daekun Kim <dkkim6200@gmail.com>
2020-03-28 23:51:12 -0700

    Kind of working-ish. Collaboration very unstable

---
+++ decor-ar/ARModelView+Collaboration.swift
@@ -0,0 +1,183 @@
+//
+//  ARModelView+Collaboration.swift
+//  decor-ar
+//
+//  Created by Daekun Kim on 2020-03-28.
+//  Copyright © 2020 DaekunKim. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+import ARKit
+import MultipeerConnectivity
+import Speech
+
+#if !targetEnvironment(simulator)
+extension ARViewModel {
+    func initCollaborationSession() {
+        // Use key-value observation to monitor your ARSession's identifier.
+        sessionIDObservation = observe(\.arView.session.identifier, options: [.new]) { object, change in
+            print("SessionID changed to: \(change.newValue!)")
+            // Tell all other peers about your ARSession's changed ID, so
+            // that they can keep track of which ARAnchors are yours.
+            guard let multipeerSession = self.multipeerSession else { return }
+            self.sendARSessionIDTo(peers: multipeerSession.connectedPeers)
+        }
+        
+        // Start looking for other players via MultiPeerConnectivity.
+        multipeerSession = MultipeerSession(receivedDataHandler: receivedData, peerJoinedHandler:
+                                            peerJoined, peerLeftHandler: peerLeft, peerDiscoveredHandler: peerDiscovered)
+        
+        // Prevent the screen from being dimmed to avoid interrupting the AR experience.
+        UIApplication.shared.isIdleTimerDisabled = true
+
+        collaborationInfoLabel = "Tap the screen to place cubes.\nInvite others to launch this app to join you."
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        print("===================================")
+        for anchor in anchors {
+            if (anchor.name != nil && !anchor.name!.isEmpty) {
+                print(anchor.name!)
+            }
+            print(anchor.sessionIdentifier == arView.session.identifier)
+            
+            if let participantAnchor = anchor as? ARParticipantAnchor {
+                collaborationInfoLabel = "Established joint experience with a peer."
+                // ...
+                let anchorEntity = AnchorEntity(anchor: participantAnchor)
+                
+                let coordinateSystem = MeshResource.generateCoordinateSystemAxes()
+                anchorEntity.addChild(coordinateSystem)
+                
+                let color = participantAnchor.sessionIdentifier?.toRandomColor() ?? .white
+                let coloredSphere = ModelEntity(mesh: MeshResource.generateSphere(radius: 0.03),
+                                                materials: [SimpleMaterial(color: color, isMetallic: true)])
+                anchorEntity.addChild(coloredSphere)
+                
+                arView.scene.addAnchor(anchorEntity)
+            }
+            else if anchor.sessionIdentifier != arView.session.identifier {
+                guard let furnitureName = anchor.name?.components(separatedBy: " ").first else {
+                    continue;
+                }
+                
+                let anchorEntity = AnchorEntity(anchor: anchor)
+                anchorEntity.name = anchor.name!
+                
+                if furnitureName == "Bookshelf" {
+                    anchorEntity.addChild(furnitureDict["Bookshelf"]!)
+                }
+                else if furnitureName == "Bookshelf-Preview" {
+                    anchorEntity.addChild(furnitureDict["Bookshelf-Preview"]!)
+                }
+                
+                arView.scene.addAnchor(anchorEntity)
+            }
+        }
+    }
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        print("===================================")
+        for anchor in anchors {
+            if anchor.sessionIdentifier != arView.session.identifier {
+                guard let anchorName = anchor.name else {
+                    continue;
+                }
+                
+                let anchorEntity = arView.scene.findEntity(named: anchorName)
+                guard let hasAnchoring = anchorEntity as? HasAnchoring else {
+                    continue;
+                }
+                arView.scene.removeAnchor(hasAnchoring)
+            }
+        }
+    }
+    
+    /// - Tag: DidOutputCollaborationData
+    func session(_ session: ARSession, didOutputCollaborationData data: ARSession.CollaborationData) {
+        guard let multipeerSession = multipeerSession else { return }
+        if !multipeerSession.connectedPeers.isEmpty {
+            guard let encodedData = try? NSKeyedArchiver.archivedData(withRootObject: data, requiringSecureCoding: true)
+            else { fatalError("Unexpectedly failed to encode collaboration data.") }
+            // Use reliable mode if the data is critical, and unreliable mode if the data is optional.
+            let dataIsCritical = data.priority == .critical
+            multipeerSession.sendToAllPeers(encodedData, reliably: dataIsCritical)
+        } else {
+            print("Deferred sending collaboration to later because there are no peers.")
+        }
+    }
+
+    func receivedData(_ data: Data, from peer: MCPeerID) {
+        if let collaborationData = try? NSKeyedUnarchiver.unarchivedObject(ofClass: ARSession.CollaborationData.self, from: data) {
+            arView.session.update(with: collaborationData)
+            return
+        }
+        // ...
+        let sessionIDCommandString = "SessionID:"
+        if let commandString = String(data: data, encoding: .utf8), commandString.starts(with: sessionIDCommandString) {
+            let newSessionID = String(commandString[commandString.index(commandString.startIndex,
+                                                                     offsetBy: sessionIDCommandString.count)...])
+            // If this peer was using a different session ID before, remove all its associated anchors.
+            // This will remove the old participant anchor and its geometry from the scene.
+            if let oldSessionID = peerSessionIDs[peer] {
+                removeAllAnchorsOriginatingFromARSessionWithID(oldSessionID)
+            }
+            
+            peerSessionIDs[peer] = newSessionID
+        }
+    }
+    
+    func peerDiscovered(_ peer: MCPeerID) -> Bool {
+        guard let multipeerSession = multipeerSession else { return false }
+        
+        collaborationInfoLabel = "Someone wants to join"
+        
+        if multipeerSession.connectedPeers.count > 3 {
+            // Do not accept more than four users in the experience.
+            collaborationInfoLabel = "A fifth peer wants to join the experience.\nThis app is limited to four users."
+            return false
+        } else {
+            return true
+        }
+    }
+    /// - Tag: PeerJoined
+    func peerJoined(_ peer: MCPeerID) {
+        collaborationInfoLabel = """
+            A peer wants to join the experience.
+            Hold the phones next to each other.
+            """
+        // Provide your session ID to the new user so they can keep track of your anchors.
+        sendARSessionIDTo(peers: [peer])
+    }
+        
+    func peerLeft(_ peer: MCPeerID) {
+        collaborationInfoLabel = "A peer has left the shared experience."
+        
+        // Remove all ARAnchors associated with the peer that just left the experience.
+        if let sessionID = peerSessionIDs[peer] {
+            removeAllAnchorsOriginatingFromARSessionWithID(sessionID)
+            peerSessionIDs.removeValue(forKey: peer)
+        }
+    }
+    
+    private func removeAllAnchorsOriginatingFromARSessionWithID(_ identifier: String) {
+        guard let frame = arView.session.currentFrame else { return }
+        for anchor in frame.anchors {
+            guard let anchorSessionID = anchor.sessionIdentifier else { continue }
+            if anchorSessionID.uuidString == identifier {
+                arView.session.remove(anchor: anchor)
+            }
+        }
+    }
+    
+    private func sendARSessionIDTo(peers: [MCPeerID]) {
+        guard let multipeerSession = multipeerSession else { return }
+        let idString = arView.session.identifier.uuidString
+        let command = "SessionID:" + idString
+        if let commandData = command.data(using: .utf8) {
+            multipeerSession.sendToPeers(commandData, reliably: true, peers: peers)
+        }
+    }
+}
+#endif
---
+++ decor-ar/ARModelView.swift
@@ -0,0 +1,219 @@
+//
+//  ARModelView.swift
+//  decor-ar
+//
+//  Created by Daekun Kim on 2020-03-28.
+//  Copyright © 2020 DaekunKim. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+import ARKit
+import MultipeerConnectivity
+import Speech
+
+final class ARViewModel: NSObject, UIViewRepresentable, ObservableObject, ARSessionDelegate {
+    @Published @objc dynamic var arView : ARView! = ARView(frame: .zero)
+    private var configuration : ARWorldTrackingConfiguration?
+    
+    @Published var recordEnabled : Bool = false
+    @Published var speechInfoLabel : String = ""
+    @Published var transcriptionLabel : String = ""
+    @Published var collaborationInfoLabel : String = ""
+    
+    @Published var placingFurniture : Bool = false
+    internal var furnitureDict : [String : Entity] = [:]
+    private var currentFurnitureName : String = ""
+    private var currentFurniture : Entity? = nil
+    private var currentFurniturePreview : Entity? = nil
+    internal var currentARAnchor : ARAnchor? = nil
+    private var currentAnchor : AnchorEntity? = nil
+    
+    internal let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    internal var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    internal var recognitionTask: SFSpeechRecognitionTask?
+    internal let audioEngine = AVAudioEngine()
+    
+    var multipeerSession: MultipeerSession?
+    // A dictionary to map MultiPeer IDs to ARSession ID's.
+    // This is useful for keeping track of which peer created which ARAnchors.
+    var peerSessionIDs = [MCPeerID: String]()
+    var sessionIDObservation: NSKeyValueObservation?
+    
+    func makeUIView(context: Context) -> ARView {
+        initARView()
+        initSpeechRecognizer()
+        initCollaborationSession()
+        
+        let furnitureScene = try! Experience.loadFurniture()
+        furnitureDict[furnitureScene.bookshelf!.name] = furnitureScene.bookshelf
+        furnitureDict[furnitureScene.bookshelfPreview!.name] = furnitureScene.bookshelfPreview
+        
+        for (i, j) in furnitureDict {
+            print("\(i) : \(j)")
+        }
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+    }
+    
+    func initARView() {
+        // Load the "Box" scene from the "Experience" Reality File
+        // Add the anchors to the scene
+//            furnitureScene.bookshelf?.isEnabled = false
+//            let bookshelfPreviewModel = findEntityWithModelComponent(e: furnitureScene.bookshelfPreview!)
+//
+//            var modelComp : ModelComponent = (bookshelfPreviewModel?.components[ModelComponent])!
+//            let mat = SimpleMaterial(color: .init(white: 1.0, alpha: 0.3), isMetallic: false)
+//            modelComp.materials = [mat]
+//            bookshelfPreviewModel?.components.set(modelComp)
+//
+//            arView.scene.anchors.append(pointerScene)
+//            arView.scene.anchors.append(furnitureScene)
+        
+        #if !targetEnvironment(simulator)
+        arView.automaticallyConfigureSession = false
+        
+        configuration = ARWorldTrackingConfiguration()
+
+        // Enable a collaborative session.
+        configuration?.isCollaborationEnabled = true
+        
+        // Enable realistic reflections.
+        configuration?.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) {
+            configuration?.frameSemantics.insert(.personSegmentationWithDepth)
+        }
+        
+        arView.session.delegate = self
+        
+        // Begin the session.
+        arView.session.run(configuration!)
+        #endif
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        #if !targetEnvironment(simulator) //==============================================================
+        switch frame.camera.trackingState {
+        case .normal:
+            if (placingFurniture) {
+                let results = arView.raycast(from: arView.center,
+                                             allowing: .estimatedPlane,
+                                            alignment: .horizontal)
+                
+                guard let result = results.first else {
+                    return
+                }
+                
+                let newAnchor = AnchorEntity()
+                newAnchor.addChild(currentFurniture!)
+                newAnchor.addChild(currentFurniturePreview!)
+                
+                let transform = Transform(matrix: result.worldTransform)
+                newAnchor.move(to: transform, relativeTo: nil)
+                
+                if (currentAnchor != nil) {
+                    newAnchor.transform.rotation = currentAnchor?.transform.rotation as! simd_quatf
+//                        newAnchor.setOrientation((currentAnchor?.transform.rotation)!, relativeTo: nil)
+                    arView.scene.removeAnchor(currentAnchor as! HasAnchoring)
+                }
+                
+                arView.scene.anchors.append(newAnchor)
+                currentAnchor = newAnchor
+                
+                let newARAnchor = ARAnchor(name: currentFurnitureName + "-Preview " + String(NSDate().timeIntervalSince1970), transform: newAnchor.transform.matrix)
+                if (currentARAnchor != nil) {
+                    arView.session.remove(anchor: currentARAnchor!)
+                }
+                arView.session.add(anchor: newARAnchor)
+                currentARAnchor = newARAnchor
+            }
+            
+        case .notAvailable:
+            return
+        case .limited(_):
+            return
+        }
+        #endif
+    }
+    
+    func addFurniture(called name_: String) {
+        let name = name_.lowercased()
+        
+        if (name == "bookshelf") {
+            placingFurniture = true
+            
+            currentFurnitureName = "Bookshelf"
+            currentFurniture = furnitureDict["Bookshelf"]?.clone(recursive: true)
+            currentFurniturePreview = furnitureDict["Bookshelf-Preview"]?.clone(recursive: true)
+            
+            let bookshelfPreviewModel = findEntityWithModelComponent(e: currentFurniturePreview!)
+
+            var modelComp : ModelComponent = (bookshelfPreviewModel?.components[ModelComponent])!
+            let mat = SimpleMaterial(color: .init(white: 1.0, alpha: 0.3), isMetallic: false)
+            modelComp.materials = [mat]
+            bookshelfPreviewModel?.components.set(modelComp)
+            
+            currentFurniture?.isEnabled = false
+            
+//                currentAnchor = AnchorEntity(plane: .horizontal)
+//                currentAnchor?.addChild(currentFurniture!)
+//                currentAnchor?.addChild(currentFurniturePreview!)
+        }
+    }
+    
+    func confirmFurniturePosition() {
+//            furnitureScene.bookshelf!.isEnabled.toggle()
+//            furnitureScene.bookshelfPreview!.isEnabled.toggle()
+        self.placingFurniture = false
+        
+        let newARAnchor = ARAnchor(name: currentFurnitureName + " " + String(NSDate().timeIntervalSince1970), transform: currentAnchor!.transformMatrix(relativeTo: nil))
+        arView.session.remove(anchor: currentARAnchor!)
+        arView.session.add(anchor: newARAnchor)
+        
+        currentFurniturePreview?.isEnabled = false
+        currentFurniture?.isEnabled = true
+        currentAnchor = nil
+        currentARAnchor = nil
+        currentFurnitureName = ""
+    }
+    
+    func setFurnitureRotation(angle: Float) {
+        currentAnchor?.transform.rotation = .init(angle: -angle, axis: SIMD3<Float>(0, 1, 0))
+    }
+    
+    @available(*, deprecated)
+    func togglePeopleOcclusion() {
+        #if !targetEnvironment(simulator)
+        guard let config = arView.session.configuration as? ARWorldTrackingConfiguration else {
+            fatalError("Unexpectedly failed to get the configuration.")
+        }
+        guard ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) else {
+//            fatalError("People occlusion is not supported on this device.")
+            return
+        }
+        switch config.frameSemantics {
+        case [.personSegmentationWithDepth]:
+            config.frameSemantics.remove(.personSegmentationWithDepth)
+        default:
+            config.frameSemantics.insert(.personSegmentationWithDepth)
+        }
+        arView.session.run(config)
+        #endif
+    }
+    
+    private func findEntityWithModelComponent(e : Entity, recursiveSearchLimit : Int = 10) -> Entity? {
+        var curEntity : Entity = e
+        
+        var searchLimitCounter : Int = 0
+        while (searchLimitCounter < recursiveSearchLimit && curEntity.components[ModelComponent] == nil) {
+            curEntity = curEntity.children[0]
+            searchLimitCounter += 1
+        }
+        
+        return searchLimitCounter >= recursiveSearchLimit ? nil : curEntity
+    }
+}
